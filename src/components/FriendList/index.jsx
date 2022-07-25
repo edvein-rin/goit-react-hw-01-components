3a276160d31911ff8e1243ae2d829007c81d07e8
edvein-rin <edveinrin@gmail.com>
2022-07-26 00:28:09 +0300

    Added `FriendList` component

---
+++ src/components/FriendList/index.jsx
@@ -0,0 +1,28 @@
+import { FriendListItem } from 'components/FriendListItem';
+import PropTypes from 'prop-types';
+
+import styles from './styles.module.css';
+
+export const FriendList = ({ friends }) => (
+  <ul className={styles.friendList}>
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+      id: PropTypes.number,
+    })
+  ).isRequired,
+};
