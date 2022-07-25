994aad776d150d87f65f1bdb6bd8beddeec7d9b1
edvein-rin <edveinrin@gmail.com>
2022-07-26 00:54:15 +0300

    Added `TransactionHistory` usage example

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,13 @@
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 import { Profile } from 'components/Profile';
 import { Statistics } from 'components/Statistics';
 import { FriendList } from 'components/FriendList';
 
 import styles from './styles.module.css';
+import { TransactionHistory } from 'components/TransactionHistory';
 
 export const App = () => {
   return (
@@ -27,6 +29,9 @@ export const App = () => {
       </div>
       <div className={styles.wrapper}>
         <FriendList friends={friends} />
+      </div>
+      <div className={styles.wrapper}>
+        <TransactionHistory items={transactions} />
       </div>
     </div>
   );
